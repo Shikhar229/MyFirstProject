2ae5a83813510a1c470f19e7ea6e9d2d1b98a2ff
Shikhar <shikhar0538@gmail.com>
2026-08-11 08:36:48 +0530

    If we want to take variable sized arguments

---
+++ src/Array/VarArgs.java
@@ -0,0 +1,33 @@
+package Array;
+
+
+public class VarArgs {
+    public static void main(String[] args) {
+//        varargs =allow a method to accept a varying # of arguments
+//        makes methods more flexible no need for overload methods java will pack the arguments into an array
+//        (ellipsis)
+        System.out.println(add(1,2,3,4));
+        System.out.println(average());
+
+
+    }
+
+    static int add(int... numbers) {
+        int sum = 0;
+        for(int number: numbers){
+            sum += number;
+        }
+        return sum;
+    }
+    static double average(double... numbers){
+        double sum = 0;
+        if(numbers.length == 0){
+            return 0;
+        }
+        for(double number: numbers){
+            sum += number;
+        }
+        return sum/numbers.length;
+    }
+
+}
